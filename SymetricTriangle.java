e7dff5743eb90e97946574f6e1da2213c9c470b0
Edia Zaki <13521141@mahasiswa.itb.ac.id>
2024-06-13 13:13:33 +0700

    feat: symetric triangle

---
+++ SymetricTriangle.java
@@ -0,0 +1,49 @@
+import java.util.Scanner;
+
+
+public class SymetricTriangle {
+    public static void main(String[] args) {
+        SymetricTriangle symetricTriangle = new SymetricTriangle();
+        Scanner scanner = new Scanner(System.in);
+        int input = 0;
+        while(input <= 0){
+            System.err.print("Enter a number: ");
+            input = scanner.nextInt();
+            if(input <= 0){
+                System.err.println("Please enter a number greater than 0");
+            }
+        }
+        scanner.close();
+        symetricTriangle.printSymetricTriangle(input);
+    }
+
+    public void printSymetricTriangle(int num){
+        printReverseTriangle(num);
+        printTriangle(num);
+    }
+
+    public void printReverseTriangle(int num){
+        for (int i = 0; i < num; i++) {
+            printLine(i, num);
+        }
+    }
+
+    public void printTriangle(int num){
+        for (int i = num - 1; i >= 0; i--) {
+            printLine(i, num);
+        }
+    }
+
+    public void printLine(int prefix, int content){
+        for (int i = 0; i < prefix; i++) {
+            System.out.print(" ");
+        }
+        for (int i = 0; i < content - prefix; i++) {
+            System.out.print("* ");
+        }
+        for (int i = 0; i < prefix; i++) {
+            System.out.print(" ");
+        }
+        System.err.print("\n");
+    }
+}
