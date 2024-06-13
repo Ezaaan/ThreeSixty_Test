671f8fb78cbee1fa6f5774c32bb017c62c4a411c
Edia Zaki <13521141@mahasiswa.itb.ac.id>
2024-06-13 13:37:15 +0700

    feat: symetric triangle number

---
+++ SymetricTriangleNumber.java
@@ -0,0 +1,45 @@
+import java.util.Scanner;
+
+public class SymetricTriangleNumber {
+    public static void main(String[] args) {
+        SymetricTriangleNumber symetricTriangle = new SymetricTriangleNumber();
+        Scanner scanner = new Scanner(System.in);
+        int input = 0;
+        while(input <= 0){
+            System.err.print("Enter a number: ");
+            input = scanner.nextInt();
+            if(input <= 0){
+                System.err.println("Please enter a number greater than 0");
+            }
+        }
+        scanner.close();
+        symetricTriangle.printSymetricTriangleNumber(input);
+    }
+
+    public void printSymetricTriangleNumber(int num){
+        for (int i = 0; i < num; i++) {
+            printLine(i+1, num);
+        }
+        for (int i = num; i > 0; i--) {
+            printLine(i-1, num);
+        }
+    }
+
+    public void printLine(int id, int num){
+        for (int i = 0; i < num; i++) {
+            if(i < id){
+                System.out.print(i+1);
+            } else {
+                System.out.print(" ");
+            }
+        }
+        for (int i = num-1; i > 0; i--) {
+            if(i <= id){
+                System.out.print(i);
+            } else {
+                System.out.print(" ");
+            }
+        }
+        System.err.print("\n");
+    }
+}
