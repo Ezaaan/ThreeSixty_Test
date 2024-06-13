a348d65da120d2cdd0e91c0b91c62fb0824cd35d
Edia Zaki <13521141@mahasiswa.itb.ac.id>
2024-06-13 13:13:15 +0700

    feat: palindrome

---
+++ Palindrome.java
@@ -0,0 +1,30 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Palindrome {
+    public static void main(String[] args) {
+        List<Character> inputs = new ArrayList<>();
+        Scanner scanner = new Scanner(System.in);
+        
+        String input = "";
+        while(!"exit".equals(input) || input.isEmpty()){
+            System.err.print("Enter a character (type \"exit\" to stop): ");
+            input = scanner.nextLine();
+            if(input.length() == 1){
+                inputs.add(input.charAt(0));
+            } else if(!"exit".equals(input)){
+                System.err.println("Please enter only one character");
+            }
+        }
+        scanner.close();
+        System.err.print("\nPalindrome: ");
+        for (int i = 0; i < inputs.size() * 2; i++) {
+            if(i < inputs.size()){
+                System.out.print(inputs.get(i));
+            } else {
+                System.out.print(inputs.get(inputs.size() - (i - inputs.size() + 1)));
+            }
+        }
+    }
+}
